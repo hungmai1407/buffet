81b998c6fcbef9156083da5bd1a83f5a91fe86a4
maisyhungns <maisyhungns@gmail.com>
2022-05-30 01:53:56 +0700

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+};
+
+export const spotifyEnv = {
+  clientId: '95ccdf518a8e4d6db9a20bb2fe84d6ad',
+  clientSecret: '1a5cea5c16b64bf784cadef9244be810',
+  authorizeUrl: 'https://accounts.spotify.com/authorize',
+  tokenUrl: 'https://accounts.spotify.com/api/token',
 };
